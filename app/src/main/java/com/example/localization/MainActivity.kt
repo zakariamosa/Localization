f67bbcc43e90764af701290843cc3be028fb098d
zakariamosa <zakaria.mosa@iths.se>
2020-09-16 10:38:46 +0200

    translate main activity layout to swedish

--- app/src/main/java/com/example/localization/MainActivity.kt
+++ app/src/main/java/com/example/localization/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.localization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+
+lateinit var greetingTextView:TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        greetingTextView=findViewById(R.id.textView)
     }
+
+
+
+    fun button_pressed(view: View){
+        greetingTextView.text=getString(R.string.great)
+    }
+
+
+
+
 }
 
 
